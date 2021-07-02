a9757518e426d9c551c7f0209012e763bbb601ed
Brody Beebe <bbeebe13@gmail.com>
2021-07-02 14:58:49 -0600

    added generate page arrow function

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const name = profileDataArgs[0];
+const github = profileDataArgs[1];
 
-const printProfileData = profileDataArr => {
+const generatePage = (userName, githubName) => {
+    return `
+    Name: ${userName}
+    Github: ${githubName}
+    `;
 
-    //This...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);
-    }
-
-    console.log('==================');
-
-    //Is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
-
-printProfileData(profileDataArgs);
+}
+console.log(generatePage(name, github));
