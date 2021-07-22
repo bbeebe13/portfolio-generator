a19621fbbba16ccf8b83607d486ce16893bfea52
Brody Beebe <bbeebe13@gmail.com>
2021-07-21 21:08:19 -0600

    added inquirer

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
+const inquirer = require('inquirer');
+//const fs = require('fs');
+//const generatePage = require('./src/page-template.js');
+//const pageHTML = generatePage(name, github);
 
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-const profileDataArgs = process.argv.slice(2);
-const [name, github] = profileDataArgs;
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
 
-
-
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
-
-    console.log('Portfolio complete! Checkout index.html to see the output!')
-});
+//fs.writeFile('index.html', generatePage(name, github), err => {
+//    if (err) throw err;
+//
+//    console.log('Portfolio complete! Checkout index.html to see the output!')
+//});
