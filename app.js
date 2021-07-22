cf7dff3a5bba61470e3bc00f740a15733f0064aa
Brody Beebe <bbeebe13@gmail.com>
2021-07-21 19:25:46 -0600

    created HTML page and modularized code

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-const name = profileDataArgs[0];
-const github = profileDataArgs[1];
 
-const generatePage = (userName, githubName) => {
-    return `
-    Name: ${userName}
-    Github: ${githubName}
-    `;
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+const profileDataArgs = process.argv.slice(2);
+const [name, github] = profileDataArgs;
 
-}
-console.log(generatePage(name, github));
+
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+
+    console.log('Portfolio complete! Checkout index.html to see the output!')
+});
